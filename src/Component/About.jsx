f527e8667481fc5a81d7a6203adb7cb3b0049959
Jihoo <wlgn829@kakao.com>
2022-05-27 13:20:56 +0900

    About 구현

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -7,7 +7,9 @@ export default function About() {
 			<div className="container">
 				<div className="about_box">
 					<div className="about_left">
-						<h2></h2>
+						<h2>
+							&lt;About me <span>type = "자기소개"</span> &gt;
+						</h2>
 					</div>
 					<div className="about_right"></div>
 				</div>
