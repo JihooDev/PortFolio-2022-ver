df15b54a96a75240d145e070261b75befb5916a2
Jihoo <wlgn829@kakao.com>
2022-06-01 14:31:33 +0900

    framer-motion

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import '../Style/main.scss';
+import { motion } from 'framer-motion';
+
 export default function Main() {
 	return (
-		<div className="Main">
+		<motion.div className="Main" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: 1.1, transition: { duration: 2, delayChildren: 3 } }}>
 			<div className="container">
 				<main>
-					<h2>
-						안녕하세요 <span>성실함 & 책임감</span>이 기본 탑재 되어있는
+					<motion.h2 animate={{ scale: 0.8, transition: { duration: 1 } }}>
+						안녕하세요 <motion.span animate={{ background: '#de3c4b', transition: { duration: 4 } }}>성실함 & 책임감</motion.span>
+						이 기본 탑재 되어있는
 						<br />
 						클라이언트 개발자 김지후 입니다
-					</h2>
+					</motion.h2>
 				</main>
 			</div>
 			<footer>
 				<p>JIHOO’S PortFolio 2022 ver</p>
 			</footer>
-		</div>
+		</motion.div>
 	);
 }
